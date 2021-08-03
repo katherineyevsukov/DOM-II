784bf97d78219332ae13ce84f50221884860baa7
Kat <kyevsukov@gmail.com>
2021-08-03 19:50:30 -0400

    finished all MVP

--- js/index.js
+++ js/index.js
@@ -25,14 +25,14 @@ busImg.addEventListener('click', imgChanger)
 // busImg.addEventListener('dblclick', imgChanger)
 
 
-//adding modal opener button
+//adding modal 
 const buttonParent = document.querySelector('.text-content h2')
 const newButton = document.createElement('button')
 newButton.textContent = 'Go!'
 buttonParent.appendChild(newButton)
 newButton.style.margin = '10%'
 
-//focus + blur events for form
+
 
 const modalBox = document.createElement('div')
 modalBox.style.width = '75%'
@@ -46,13 +46,18 @@ modalBox.style.position ='absolute'
 const form = document.createElement('form')
 const inputOne = document.createElement('input')
 const inputTwo = document.createElement('input')
+const formButton = document.createElement('button')
+formButton.textContent = 'Submit'
+
 
 modalBox.appendChild(form)
 form.appendChild(inputOne)
 form.appendChild(inputTwo)
-
+form.appendChild(formButton)
 form.style.display = 'flex'
 form.style.flexDirection = 'column'
+
+//focus + blur events for form
 
 const addPink = ( e => {
     e.target.style.backgroundColor = 'lightpink'
@@ -69,12 +74,56 @@ const parent = document.querySelector('.content-section');
 console.log(parent)
 parent.prepend(modalBox);
 
-newButton.addEventListener('click', e => {
+newButton.addEventListener('dblclick', e => {
     modalBox.style.display = 'flex'
 })
+
+// escape button causes modal to close
 
 document.addEventListener('keydown', (e) => {
     if (e.keyCode == 27 ){
         modalBox.style.display = 'none'
     }
 })
+
+//welcome alert when page loads
+
+window.addEventListener('load', (e) => {
+    alert('Welcome to the Fun Bus Page!')
+})
+
+// nav bar changes color when mouse enters or leaves
+
+const navBar = document.querySelectorAll('nav a')
+
+const navBarArray = Array.from(navBar)
+console.log(navBarArray)
+
+const nav = document.querySelector('nav')
+
+nav.addEventListener('mouseenter', (e) => {
+    navBarArray.forEach(nav => nav.style.color ='red')
+})
+
+nav.addEventListener('mouseleave', (e) => {
+    navBarArray.forEach(nav => nav.style.color = 'black')
+})
+
+//changing background color on double click
+
+let background = document.querySelector('body')
+background.addEventListener('dblclick', (e) => {
+    background.style.backgroundColor = 'hotpink'
+})
+
+//PROPAGATION to prevent background from changing color on button click
+
+newButton.addEventListener('dblclick', e => {
+    e.stopPropagation()
+})
+
+//PREVENTDEFAULT avoiding refresh of page upon form submission
+
+formButton.addEventListener('click', (e) =>
+    e.preventDefault()
+)
